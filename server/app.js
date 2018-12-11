00fdfd4f247f4ecf2dc11fa602f058cf713f064e
Thibault Moussy <thibaultmoussy123@gmail.com>
2018-12-11 10:09:08 +0100

    change port

--- server/app.js
+++ server/app.js
@@ -26,6 +26,6 @@ app.get('/about-us', function (req, res) {
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
